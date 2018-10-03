03f09df5ed0537fc144e2cb6a3950c2b07e13f00
Cristian <cdcscooter@gmail.com>
2018-10-03 15:45:37 +0200

    Creació del projecte i creació del constructor a la classe Calculadora2.java

---
+++ src/calculadora2/Calculadora2.java
@@ -0,0 +1,13 @@
+package calculadora2;
+public class Calculadora2 {
+    private int num1 = 6;
+    private int num2 = 24;
+    public Calculadora2(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
